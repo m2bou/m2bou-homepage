f25f81a7c97dfaa194e036fd288589e11950e19a
m2bou <g.s.mon923@gmail.com>
2026-08-24 23:51:49 +0900

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,4 +1,10 @@
-import { onRequestGet as postHandler } from "./functions/posts/[id].js";
+import {
+  onRequestGet as postHandler
+} from "./functions/posts/[id].js";
+
+import {
+  onRequestGet as postIndexHandler
+} from "./functions/posts/index.js";
 
 export default {
   async fetch(request, env, ctx) {
@@ -9,6 +15,23 @@ export default {
       return new Response("Workers routing OK");
     }
 
+    // /posts 一覧
+
+    if (
+      url.pathname === "/posts" ||
+      url.pathname === "/posts/"
+    ) {
+
+      return postIndexHandler({
+        request,
+        env,
+        params: {},
+        waitUntil:
+          ctx.waitUntil.bind(ctx)
+      });
+
+    }
+    
     // /posts/12345678
     const postMatch =
       url.pathname.match(/^\/posts\/(\d+)\/?$/);
